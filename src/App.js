5472e5a400461d574236ff9f9159715c5bff0684
MPak9 <56807392+MPak9@users.noreply.github.com>
2024-01-16 13:31:30 -0500

    Flippable Card CSS & Oganization

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isFlipped, setIsFlipped] = useState(false);
 
   return (
     <div className="App">
-      <div className='cardContainer frontCard'>
-        <div className='outline'> 
-          <div className='contentContainer'>a</div>
-        </div>
-        <div className='outline'> 
-          <div className='contentContainer' >
-            <div className='headerContainer' >a</div>
-            <div >b</div>
-            <div >c</div>
+      <button onClick={
+        (e) => { 
+          e.preventDefault();  
+          setIsFlipped(prev => !prev);
+        }
+      }>
+          FLIP!
+      </button>
+      <br/>
+      <div className={`flippableCardContainer ${isFlipped ? 'isFlipped' : ''}`}>
+        <div className={`card`}>
+          <div className='frontCard'>
+            <div className=''> 
+              <div className='contentContainer'>a</div>
+            </div>
+            <div className=''> 
+              <div className='contentContainer' >
+                <div className='headerContainer' >a</div>
+                <div >b</div>
+                <div >c</div>
+              </div>
+            </div>
           </div>
+          
+          <div className='backCard'>
+            <div className=''> 
+              <div className='contentContainer'>a</div>
+            </div>
+            <div className=''> 
+              <div className='contentContainer' >
+                <div className='headerContainer' >a</div>
+                <div >b</div>
+                <div >c</div>
+              </div>
+            </div>
+          </div>
+
+          
         </div>
       </div>
+      
     </div>
   );
 }
